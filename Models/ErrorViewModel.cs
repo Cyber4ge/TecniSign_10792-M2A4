d41d61ba9ead5f5c1053c079cfa6527288faf0d2
Cyber4ge <127634111+Cyber4ge@users.noreply.github.com>
2023-04-25 14:24:30 +0100

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TecniSign_10792_M2A4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
